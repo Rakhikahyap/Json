da458df8053bd30e1534c7bb1193aa6574cf4470
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-03-10 18:59:17 +0530

    Add files via upload

---
+++ meraki3.py
@@ -0,0 +1,12 @@
+# import json
+# py_ob = {
+#   "name": "John",
+#   "age": 30,
+#   "city": "New York"
+# }
+# json_str=json.dumps(py_ob)
+# print(json_str,type(json_str))
+# a="priya"
+# b=a.join()
+# print(b)
+
